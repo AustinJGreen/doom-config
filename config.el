445cdb456f7e5612b5d1ff777fbd1bfaa74d56d2
Austin Green <agreen@liftoff.io>
2020-12-13 18:07:31 -0800

    Group setq

--- config.el
+++ config.el
@@ -1,61 +1,29 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;;
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
 
 ;; TODO(Austin): Migrate to emacs 27 when this is fixed?
 ;; https://github.com/hlissner/doom-emacs/issues/1170
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Austin Green"
-      user-mail-address "agreen@liftoff.io")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-outrun-electric)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-;; Begin Austin
+      user-mail-address "agreen@liftoff.io"
+      doom-theme 'doom-outrun-electric
+      org-directory "~/org/"
+      display-line-numbers-type nil
+      ; Add go commands
+      gofmt-command "goimports"
+      gofmt-args '("-local" "liftoff/")
+      ; Cider config (for my packages)
+      ; (setq cider-required-middleware-version "0.25.5")
+      ; (setq cider-inject-dependencies-at-jack-in nil)
+      cider-clojure-cli-global-options "-A:liftoff:dev:nrepl"
+      ;; Increase size of recentf
+      recentf-max-menu-items 25
+      recentf-max-saved-items 200
+      projectile-project-search-path "${REPOS}/liftoff/"
+      centaur-tabs-height 16
+      centaur-tabs-set-close-button nil)
 
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
@@ -102,25 +70,11 @@
 
 (sync-env)
 
-(setq projectile-project-search-path "${REPOS}/liftoff/")
-
-; Cider config (for my packages)
-; (setq cider-required-middleware-version "0.25.5")
-; (setq cider-inject-dependencies-at-jack-in nil)
-(setq cider-clojure-cli-global-options "-A:liftoff:dev:nrepl")
 
 ;; Enable formatting on save
 (load! "$REPOS/liftoff/exp/emacs/cljfmt.el")
 (add-hook 'before-save-hook 'cljfmt-before-save)
-
-;; Add go commands
-(setq gofmt-command "goimports")
-(setq gofmt-args '("-local" "liftoff/"))
 (add-hook 'before-save-hook 'gofmt-before-save)
-
-;; Increase size of recentf
-(setq recentf-max-menu-items 25)
-(setq recentf-max-saved-items 200)
 
 ; Use diff-hl-mode always
 (global-diff-hl-mode)
@@ -133,6 +87,6 @@
 (map! :leader
       :desc "Go to right tab"
       "<right>" #'centaur-tabs-forward)
-(setq centaur-tabs-height 16)
-(setq centaur-tabs-set-close-button nil)
+
+; Enable centuar mode by default
 (centaur-tabs-mode)
